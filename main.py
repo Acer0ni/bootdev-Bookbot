a427dca47c99982aba71a5d13d04d35e7e044212
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2024-03-20 17:47:01 -0400

    finished bookbot

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
         words = file_contents.split()
-        print(len(words))
+        print(f"{len(words)} words found in the document")
+        char_dict = count_letters(file_contents)
+        for letter in char_dict:
+            print(f"{letter} character was found {char_dict[letter]}")
 
 def count_letters(target_string):
     lowered_string = target_string.lower()
