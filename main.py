fc241f5bdb12a4efcaa7be05bc0ec41ee566f43e
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2024-03-20 17:28:18 -0400

    added count letter function

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ def main():
         words = file_contents.split()
         print(len(words))
 
+def count_letters(target_string):
+    lowered_string = target_string.lower()
+    answer_dict = {}
+    for letter in lowered_string:
+        if letter in  answer_dict:
+            answer_dict[letter] += 1
+        else:
+            answer_dict[letter] = 1
+    return answer_dict
+
+
 
 main()
